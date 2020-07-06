95976882bdef3ea5808caa4ffde880e2c4d42d28
Joey <joey917@uw.edu>
2020-07-05 17:38:54 -0700

    More work on quiz mode

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Flashcard from './Flashcard';
 
 export default function FlashcardList({ flashcards }){
-    const currCard = flashcards[0];
-    if(currCard == null){
+    const [flashcard, setFlashcard] = useState(flashcards[0]);
+    const [numCorrect, setNumCorrect] = useState(0);
+    const [numAnswered, setNumAnswered] = useState(0);
+    const questionEl = useRef();
+
+    //setFlashcard(queue.shift());
+
+    if(flashcard == null){
         return null;
     }
+    var correctAnswer = flashcard.options.indexOf(flashcard.answer);
     return(
         <div>
-            <div className="question">{currCard.question}</div>
+            <div className="question">{flashcard.question}</div>
             <div className="quiz-options">
-                {currCard.options.map((option, index)=>{
-                            return <div id={"q" + index} className="quiz-option" key={option}>{option}</div>
+                {flashcard.options.map((option, index)=>{
+                            return <button onClick={() => checkAnswer(index)} id={"q" + index} className="quiz-option" key={option}><p>{option}</p></button>
                 })}
             </div>
         </div>
     )
+
+    function checkAnswer(index){
+        if(index == correctAnswer){
+            setNumCorrect(numCorrect + 1);
+            //queue.push(flashcard);
+        }
+        setNumAnswered(numAnswered + 1);
+        if(numAnswered >= flashcards.length){
+            console.log("Out of questions! You got " + numCorrect + " out of " + numAnswered + " correct.");
+        }
+        setFlashcard(flashcards[numAnswered]);
+    }
+    
 }
