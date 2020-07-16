6247b5987c41aec63a50c0c0c6969ae3910ed605
Joey <joeyellis917@yahoo.com>
2020-07-16 03:11:18 -0700

    Minor changes to authorization

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef, Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
@@ -41,6 +41,9 @@ function App() {
           <Route exact path ="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component= {Dashboard} />
+          </Switch>
         </Router>
       </Provider>
     </>
